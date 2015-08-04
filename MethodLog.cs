781376c29456ab1e8b9458a72f8cd1074f79306f
Derek Hearn <derek@meetball.com>
2015-08-03 21:21:51 -0500

    view lots of stuff

--- MethodLog.cs
+++ MethodLog.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DBDataAccess;
 using ResultWrappers;
 
 namespace DataAnalysis
@@ -33,8 +32,7 @@ namespace DataAnalysis
 			{
 				DateTime start = day.AddHours(-12);
 				DateTime end = day.AddHours(12);
-				var query = DBDataAccess.MethodLog
-					.Query(log => log.CallDate > start && log.CallDate < end);
+                var query = getAllWithinDateRange(start, end);
 
 				var otherCount = query.Count();
 
@@ -53,8 +51,7 @@ namespace DataAnalysis
 				var utcnow = DateTime.UtcNow;
 				DateTime start = DateTime.Today.AddDays(-daysBack);
 				DateTime end = utcnow;
-				var query = DBDataAccess.MethodLog
-					.Query(log => log.CallDate > start && log.CallDate < end);
+                var query = getAllWithinDateRange(start, end);
 
 				var list = new List<int>();
 
@@ -65,8 +62,7 @@ namespace DataAnalysis
 					list.Add(query.Count(log=> 
 						log.CallDate > t_start &&
 						log.CallDate < t_end));
-					//Console.WriteLine(String.Format("{2} {3} - {0}, {1}", i, list[i], t_start, t_end));
-				}
+                }
 
 				return new BLResult<int[]>(list.ToArray());
 			}
@@ -75,5 +71,78 @@ namespace DataAnalysis
 				return new BLResult<int[]>(e);
 			}
 		}
+
+        public BLResult<KeyValuePair<string, double[]>[]> durationsByMethod(int daysBack)
+        {
+            try
+            {
+                var utcnow = DateTime.UtcNow;
+                DateTime start = DateTime.Today.AddDays(-daysBack);
+                DateTime end = utcnow;
+                var query = getAllWithinDateRange(start, end);
+                
+                var MethodNames = query.Select(log=>
+                    log.MethodLogName).Distinct().ToArray();
+
+                var tasks = new Task<KeyValuePair<string, double[]>>[MethodNames.Length];
+
+                var results = new KeyValuePair<string, double[]>[MethodNames.Length];
+
+                for (int i = 0; i < MethodNames.Length; i++)
+                {
+                    Console.WriteLine("-{0}", MethodNames[i]);
+                    var tempI = i;
+
+                    var query2 = query.Where(log =>
+                        log.MethodLogName == MethodNames[tempI]);
+
+                    var averages = new double[daysBack + 1];
+
+                    for (int j = 0; j <= daysBack; j++)
+                    {
+                        var t_start = utcnow.AddDays(-(1 + j));
+                        var t_end = t_start.AddDays(1);
+                        var q3 = query2
+                            .Where(log => log.CallDate > t_start
+                                && log.CallDate < t_end)
+                            .Select(log => log.Duration)
+                            .ToArray();
+
+                        double averageCount = 0;
+                        int count = 0;
+
+                        for (int k = 0; k < q3.Length; k++)
+                        {
+                            if (q3[k].HasValue)
+                            {
+                                averageCount += q3[k].Value;
+                                count++;
+                            }
+                        }
+
+                        averages[j] = averageCount / count;
+                        Console.WriteLine("--{0} - {1:0.00}ms", MethodNames[i], averages[j]);
+                    }
+                    results[i] = new KeyValuePair<string, double[]>(
+                        MethodNames[tempI], averages);
+                }
+                
+                for (int i = 0; i < tasks.Length; i++)
+                    results[i] = tasks[i].Result;
+
+                return new BLResult<KeyValuePair<string, double[]>[]>(results);
+            }
+            catch (Exception e)
+            {
+                return new BLResult<KeyValuePair<string, double[]>[]>(e);
+            }
+        }
+
+        private IQueryable<DBDataAccess.MethodLog> getAllWithinDateRange(
+            DateTime min, DateTime max)
+        {
+            return DBDataAccess.MethodLog.Query(log => 
+                log.CallDate > min && log.CallDate < max);
+        }
 	}
 }
--- Program.cs
+++ Program.cs
@@ -13,39 +13,71 @@ namespace DataAnalysis
 			var ml = new MethodLog();
 
 			var today = DateTime.Today.AddHours(12);
-			var numOfDays = 30;
+			var numOfDays = 5;
 
-			var result = ml.numberOfCalls(numOfDays);
-			if (result.wasSuccess())
-			{
-				int averageChangeCount = 0;
-				int averageCallCount = 0;
-				int[] array = new int[result.Item.Length - 1];
-				for (int i = 0; i < result.Item.Length; i++)
-				{
-					averageCallCount += result.Item[i];
-					Console.Write(String.Format("{0} days ago - Total calls: {1}", 
-						i ,result.Item[i]));
-					if (i + 1 < result.Item.Length)
-					{
-						var delta = result.Item[i] - result.Item[i+1];
-						array[i] = delta;
-						averageChangeCount += delta;
-						var change = "SAME";
-						if(delta > 0)
-							change = "UP";
-						if(delta < 0)
-							change = "DOWN";
-						Console.WriteLine(String.Format(" - {1}: {0}",delta, change));
-					}
-				}
+            //durationsByMethod
 
-				var avgCalls = averageCallCount / result.Item.Length;
-				var avgDelta = averageChangeCount / array.Length;
-				Console.WriteLine(String.Format("\nTrend: {0}\tAverage Calls: {1}", avgDelta, avgCalls));
-			}
-			else
-				Console.WriteLine(result.MBResult.developerErrorMsg);
+            var result = ml.durationsByMethod(numOfDays);
+            if (result.wasSuccess())
+            {
+                for (int i = 0; i < result.Item.Length; i++)
+                {
+                    Console.WriteLine(String.Format("Method Name: {0}",
+                        result.Item[i].Key));
+
+                    var averageDur = result.Item[i].Value;
+
+                    for (int j = 0; j < averageDur.Length; j++)
+                    {
+                        if (i + 1 < result.Item.Length)
+                        {
+                            Console.WriteLine(String.Format("{0} days ago - {0}", j, averageDur[j]));
+
+                            var delta = averageDur[i] - averageDur[i + 1];
+                            var change = "SAME";
+                            if (delta > 0)
+                                change = "UP";
+                            if (delta < 0)
+                                change = "DOWN";
+                            Console.WriteLine(String.Format(" - {1}: {0}", delta, change));
+                        }
+                    }
+                }
+            }
+            else
+                Console.WriteLine(result.MBResult.developerErrorMsg);
+
+            //var result = ml.numberOfCalls(numOfDays);
+            //if (result.wasSuccess())
+            //{
+            //    int averageChangeCount = 0;
+            //    int averageCallCount = 0;
+            //    int[] array = new int[result.Item.Length - 1];
+            //    for (int i = 0; i < result.Item.Length; i++)
+            //    {
+            //        averageCallCount += result.Item[i];
+            //        Console.Write(String.Format("{0} days ago - Total calls: {1}", 
+            //            i ,result.Item[i]));
+            //        if (i + 1 < result.Item.Length)
+            //        {
+            //            var delta = result.Item[i] - result.Item[i+1];
+            //            array[i] = delta;
+            //            averageChangeCount += delta;
+            //            var change = "SAME";
+            //            if(delta > 0)
+            //                change = "UP";
+            //            if(delta < 0)
+            //                change = "DOWN";
+            //            Console.WriteLine(String.Format(" - {1}: {0}",delta, change));
+            //        }
+            //    }
+
+            //    var avgCalls = averageCallCount / result.Item.Length;
+            //    var avgDelta = averageChangeCount / array.Length;
+            //    Console.WriteLine(String.Format("\nTrend: {0}\tAverage Calls: {1}", avgDelta, avgCalls));
+            //}
+            //else
+            //    Console.WriteLine(result.MBResult.developerErrorMsg);
 
 			Console.ReadLine();
 		}
